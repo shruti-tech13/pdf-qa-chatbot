2f0e17991f773e47cc173ee6daa5c207b3903b09
shruti-tech13 <Shrutiiiraut13@gmail.com>
2026-08-02 21:44:16 +0530

    Update pdf_processor.py

--- utils/pdf_processor.py
+++ utils/pdf_processor.py
@@ -11,7 +11,7 @@ from typing import List
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.schema import Document
+from langchain_core.documents import Document
 
 
 def load_pdfs(uploaded_files) -> List[Document]:
